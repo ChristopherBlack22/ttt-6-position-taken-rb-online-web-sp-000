d0d173301fb9af8130eddf737c8cf12115bbd196
Christopher Black <christopher.black.2209@googlemail.com>
2021-03-21 15:54:37 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,5 +3,5 @@
 #end
 
 def position_taken?(board, index)
-!(board[index] == " " || board[index]= "" || board[index] == nil)
+!(board[index] == " " || board[index] == "" || board[index] == nil)
 end 
